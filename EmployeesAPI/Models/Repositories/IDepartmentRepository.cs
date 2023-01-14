c3db9480161a2a3c975fd2afd5c787c67e61068c
Fatma <chandoulfatma20598@gmail.com>
2023-01-14 12:12:36 +0100

    first commit

---
+++ EmployeesAPI/Models/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesAPI.Models.Repositories
+{
+   public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
+    }
+}
